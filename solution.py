9fb8a28103a9ee608e3545b131ae20def3cd4274
Manisha <manishasalavadi9514@msitprogram.net>
2020-07-13 18:52:27 +0530

    Jul 13  18:52:26

--- solution.py
+++ solution.py
@@ -2,14 +2,15 @@
 # The program should return true or false
 
 def checkArmstrong(num):
-        # Your code goes here
-        l=len(num)
-        t = num
-        sum=0
+    # Your code goes here
+    l = len(str(num))
+    t = num
+    for i in range(0, l):
+        sum = 0
         a = t % 10
         sum = sum + (a ** l)
         t = t / 10
         if sum == num:
-                return True
+            return True
         else:
-                return False
+            return False
